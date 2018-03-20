fcb83dc31316f2a667c8944c7248e31646ec8fe8
michael <mtrazzi@student.42.fr>
2018-03-20 22:32:39 +0100

    changed some styling

--- Django/projet/optimizer/urls.py
+++ Django/projet/optimizer/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     re_path(r'^accueil/', views.home),
     re_path(r'^article/(?P<id_article>.+)/', views.view_article),
     path('date/', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition),
+    path('landingpage/', views.landingpage),
+    path('githubrepo/', views.githubrepo),
 ]
