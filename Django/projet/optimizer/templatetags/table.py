f05bf676eaaf424ab581c99bde58f3e47d9c6e1f
michael <mtrazzi@student.42.fr>
2018-04-09 13:35:51 +0200

    bootstrap. Trying to ouput csv

---
+++ Django/projet/optimizer/templatetags/table.py
@@ -0,0 +1,13 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+import pandas as pd
+from django.conf import settings
+import os
+import csv
+
+register = template.Library()
+
+@register.simple_tag
+def print_table_html():
+    path = os.path.join(settings.BASE_DIR, "resources/data.csv")
+    return [line.rstrip('\n').split(',') for line in open(path)]
