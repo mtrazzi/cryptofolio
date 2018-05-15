0565535053bd3b2747632e7b84a8324f674ff52f
Julien Denes <julien50@sfr.fr>
2018-05-15 18:25:02 +0200

    Modified omega into coin nb + added value computation

--- PGPortfolio/main.py
+++ PGPortfolio/main.py
@@ -7,15 +7,14 @@ import pandas as pd
 import tensorflow as tf
 from datetime import datetime
 from random import randint
-from pgportfolio.tools.configprocess import parse_time
+from pgportfolio.tools.configprocess import parse_time, load_config
 from pgportfolio.marketdata.poloniex import Poloniex
 from pgportfolio.learn.nnagent import NNAgent
 from pgportfolio.tools.data import get_type_list, get_chart_until_success
-from pgportfolio.tools.configprocess import load_config
 
 # from pgportfolio.tools.trade import calculate_pv_after_commission
 global valid_algorithms
-valid_algorithms = ['anticor1', 'bcrp', 'best', 'bk', 'cornk', 'crp', 'cwmr_std', 'eg', 'm0', 'olmar', 'pamr', 'rmr', 'sp', 'ubah', 'up', 'wmamr']
+valid_algorithms = ['anticor1', 'bcrp', 'best', 'bk', 'cornk', 'crp', 'cwmr_std', 'eg', 'm0', 'olmar', 'olmar2', 'pamr', 'rmr', 'sp', 'ubah', 'up', 'wmamr']
 
 # Given a Poloniex object (to read Poloniex API), outputs a dataframe with coins infomation
 def create_coinlist():
@@ -78,7 +77,7 @@ def create_history(coin_list, chosen_coins, start, end, period, features):
     return res
 
 # Given the chosen network, coins, date, and current portfolio, outputs a new one
-def predict_portfolio(algo, chosen_coins, date, old_omega):
+def predict_portfolio(algo, investment, chosen_coins, date, old_omega):
 
     # Set config
     if algo.isdigit():
@@ -93,17 +92,17 @@ def predict_portfolio(algo, chosen_coins, date, old_omega):
     features = get_type_list(feature_number)
 
     # Set start and end times for retrieving data
+    date = date - 300*(date%period < 300)
+    end = int(date - (date%period)) # converts into closest round time, minus 5 min if too close from it
+
     if algo.isdigit():
         net_dir = "./train_package/" + algo + "/netfile"
-        end = int(date - (date%period)) # converts into closest smaller interval
         start = end - volume*period # setting start to 30 periods before end
         start = int(start - (start%period))
     elif algo in ['bcrp', 'best']:
-        end = int(date - (date%period)) # converts into closest smaller interval
         start = end - 90*volume*period # setting start to 2700 periods before end (special algos)
         start = int(start - (start%period))
     else:
-        end = int(date - (date%period)) # converts into closest smaller interval
         start = end - volume*period # setting start to 30 periods before end
         start = int(start - (start%period))
 
@@ -124,12 +123,43 @@ def predict_portfolio(algo, chosen_coins, date, old_omega):
         y = (history[0, :, 1:] / history[0, :, :-1])
         y = np.concatenate((np.ones((1, y.shape[1])), y), axis=0)
         omega = myAgent.decide_by_history(y.T, old_omega)
-
     else:
         print("Invalid algorithm")
         omega = None
 
-    return omega
+    # Current BTC per Currency price (transformed into Currency per BTC)
+    v = 1.0 / history[0, :, -1]
+    v = np.concatenate((np.zeros(1), v), axis=0)
+    # Round omega to percentage
+    round_omega = np.around(omega, decimals=1)
+    # Compute omega in term of currencies, not percentage
+    portfolio = investment * round_omega * v
+    return portfolio
+
+
+# Computes value of a portfolio, given either in percentage or number of coins at any given date
+def compute_value(portfolio, chosen_coins, investment, date):
+
+    # Get data
+    date = date - 300
+    end = int(date - (date%300))
+    start = end - 300
+    period = 300
+    features = ['close']
+    coin_list = create_coinlist() # a pandas dataframe with coins (index), pair, volume, price (in BTC)
+    history = create_history(coin_list, chosen_coins, start, end, period, features) # history matrix
+
+    # If in proportions: transform to coin numbers
+    if np.around(np.sum(portfolio)) == 1.0:
+        v = 1.0 / history[0, :, -1]
+        v = np.concatenate((np.zeros(1), v), axis=0)
+        portfolio = investment * portfolio * v
+
+    # If (or when) in term of coin number: return value in BTC
+    v = history[0, :, -1]
+    v = np.concatenate((np.zeros(1), v), axis=0)
+    res = np.dot(portfolio, v)
+    return np.around(res, decimals=10)
 
 def main():
     # Chose time (should be "now")
@@ -141,17 +171,25 @@ def main():
     # The list of all implemented valid_algorithms, '8' is a trained NN (example)
     print("Available algorithms:", ['8'] + valid_algorithms)
     # A fake choice of algorithm, should be chosen by the user
-    algo = '8'
+    algo = 'pamr'
     # A fake portfolio, should be the cryptocurrencies chosen by the user
     chosen_coins = ['reversed_USDT', 'ETH', 'XRP', 'STR', 'XMR', 'LTC', 'BCH', 'DASH', 'BTS', 'XEM', 'ETC']
     # A fake old omega (portfolio repartition), again should be previous one or chosen by user
-    omega = [randint(0,100) for x in range(len(chosen_coins)+1)]
-    omega = np.array([x/sum(omega) for x in omega])
+    omega = np.zeros(len(chosen_coins)+1)
+    omega[0] = 1
+    # A fake amount of BTC to invest at the beginning
+    investment = 0.04785
     ##### THIS IS THE VERY IMPORTANT FUNCTION #####
-    new_omega = predict_portfolio(algo=algo, chosen_coins=chosen_coins, date=now, old_omega=omega)
+    new_omega = predict_portfolio(algo=algo, investment=investment, chosen_coins=chosen_coins, date=int(now), old_omega=omega)
     ###############################################
-    print("Omega:", new_omega)
+    # Set commission, 0.0025 if Poloniex
+    commission_rate = 0.0025
+    # Compute value and print
+    investment = compute_value(portfolio=new_omega, chosen_coins=chosen_coins, investment=investment, date=now)
+    print("New omega is:", new_omega, "with value:", investment)
     print("Computation time:", time.time()-now)
+    
+    return new_omega
 
 if __name__ == "__main__":
     main()
